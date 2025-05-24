39285ed2f22e37b5a50d48c5e146cfed58c6a1d5
autumn903 <autumnaichat@gmail.com>
2025-05-24 21:10:03 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -759,3 +759,26 @@
 					});
 
 })(jQuery);
+/*
+chat gpt 
+*/
+const slider = document.getElementById('slider');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+
+let currentSlide = 0;
+
+nextBtn.addEventListener('click', () => {
+  if (currentSlide < slider.children.length - 1) {
+    currentSlide++;
+    slider.style.transform = `translateX(-${currentSlide * 100}vw)`;
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  if (currentSlide > 0) {
+    currentSlide--;
+    slider.style.transform = `translateX(-${currentSlide * 100}vw)`;
+  }
+});
+
